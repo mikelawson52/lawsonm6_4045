7293cdc567b9f64dfbd9c230a65acab561ed02f2
Administrator <Administrator@DESKTOP-099UQRG>
2020-02-17 13:47:38 -0500

    Initial commit of springboot project.

---
+++ src/test/java/com/assignment0/assignment0/Assignment0ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.assignment0.assignment0;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Assignment0ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
